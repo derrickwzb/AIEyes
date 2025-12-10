5cd2b7e028670d8efa371a529dfe0804b626964d
Derrick <derrick992110@gmail.com>
2025-12-10 19:29:37 +0800

    Added main.py
    Implemented the main function and voice speech to text capabilities using pyaudio  and openai whisper

--- main.py
+++ main.py
@@ -1,8 +1,52 @@
 from webcam import WebcamStream
+from assistant import Assistant
 import cv2
+from dotenv import load_dotenv
+import os
 
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables.history import RunnableWithMessageHistory
+# from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+from speech_recognition import Microphone, Recognizer, UnknownValueError
+
+
+load_dotenv()
 
 wc_stream = WebcamStream().start()
+
+def audio_callback(recognizer, audio):
+    try:
+        prompt = recognizer.recognize_whisper(audio, model="base", language="english")
+        assistant.answer(prompt, wc_stream.read(encode=True))
+
+    except UnknownValueError:
+        print("There was an error processing the audio.")
+
+
+
+# while True:
+#     cv2.imshow("webcam", wc_stream.read())
+#     if cv2.waitKey(1) in [27, ord("q")]:
+#         break
+
+# wc_stream.stop()
+# cv2.destroyAllWindows()
+
+model = ChatGoogleGenerativeAI(model= "gemini-2.5-flash")
+
+assistant = Assistant(model)
+
+recognizer = Recognizer()
+
+microphone = Microphone()
+
+with microphone as source:
+    recognizer.adjust_for_ambient_noise(source)
+
+stop_listening = recognizer.listen_in_background(microphone,audio_callback)
 
 while True:
     cv2.imshow("webcam", wc_stream.read())
@@ -11,3 +55,4 @@ while True:
 
 wc_stream.stop()
 cv2.destroyAllWindows()
+stop_listening(wait_for_stop=False)
